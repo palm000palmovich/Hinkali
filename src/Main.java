745c9062ca62d8e8d3ad8e0498f8837cb2c87176
palm000palmovich <ivankiselev6534@gmail.com>
2024-06-25 16:24:16 +0300

    ex3

--- src/Main.java
+++ src/Main.java
@@ -22,5 +22,16 @@ public class Main {
             System.out.println("Сегодня тепло, можно идти без шапки");
         }
 
+        for (int i = 0; i < 3; i++){
+            System.out.println("\n");
+        }
+        System.out.println("Задание 3");
+        int speed = 60;
+        if (speed > 60){
+            System.out.println("Если скрость " + speed + " км/ч, придется заплатить штраф.");
+        } else
+            System.out.println("Если скрость " + speed + " км/ч, можно ездить спокойно.");
+
+
     }
 }
